7240d0cb96b09d8f15e9d74a22dbad868f66298b
AndresParedes3 <andresparedes3019@gmail.com>
2023-08-06 22:57:27 -0500

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Vision' },
+  { id: 2, name: 'Thor' },
+  { id: 3, name: 'Hulk' },
+  { id: 4, name: 'Dr. Strange' },
+  { id: 5, name: 'Spiderman' },
+  { id: 6, name: 'Iron Man' },
+  { id: 7, name: 'Capitan America' },
+  { id: 8, name: 'Ant Man' },
+  { id: 9, name: 'Black Widow' },
+  { id: 10, name: 'Scarlet Witch' },
+  { id: 11, name: 'Black Panther' },
+  { id: 12, name: 'Deadpool' }
+];
